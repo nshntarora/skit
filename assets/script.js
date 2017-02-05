176beb23dd1991398a7d1417e76e6edc5b61cb37
Nishant Arora <na.nishantarora@gmail.com>
2017-02-05 23:23:10 +0530

    Code cleanup

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,21 @@
 $(document).on('click keyup',"#content",function(e){
-      //if(e.which==13)
-          //document.execCommand('insertHTML', false, '<p></p>');
-      //return false;
       e.preventDefault();
-      //if($(".default").remove()){
-      //  console.log("P");
-        $(".placeholder").remove();
-      //}
-      //else
-      //  console.log("A");
+
+      $(".placeholder").remove(); // Removes the placeholder text
+
       if($('#content').html()=="") {
-        console.log("empty");
+        // If the div becomes empty add a new paragraph
         $("#content").html("<p><span><br></span></p>");
       }
-      var text = window.getSelection();
-      var ol = "";
+
+      // Toolbar Display
+      var text = window.getSelection(); // Gets the currently selected text
       if(text.toString()!=""){
+        /* If the selected text is not empty calculate the position of the
+        selected text and accordingly set the position of the toolbar
+        */
         oRange = text.getRangeAt(0);
         oRect = oRange.getBoundingClientRect();
-        //$("body").append("<div class='tooltip'>This is a tooltip</div>");
         $(".tooltip").css("top",oRect.top-50);
         $(".tooltip").css("left",oRect.left-10);
         $(".tooltip").show();
@@ -26,91 +23,85 @@ $(document).on('click keyup',"#content",function(e){
       else
         $(".tooltip").hide();
 });
-//document.execCommand('defaultParagraphSeparator', false, 'p');
+
 $(document).ready(function(){
-  console.log("doc ready");
 
-  $("body").on("click",function(){
-    $(".tooltip").hide();
-  })
-
+  // Initiates sortable widget from Jqeury UI (/assets/jquery-ui.js)
   $('#content').sortable({
-    revert: true,
-    cancel: "#content p span"
+    revert: true,  // adds animation
+    cancel: "#content p span" // Prevents sorting is you're on the target <span>
   });
-  //$('#content').enableSelection();
+
+  // If clicked on the <p> tag bring the focus to the editor
   $(document).on("click","p",function(e){
     e.preventDefault();
     $("#content").focus();
   });
 
+  // Makes selected text bold
   $("#bold").on("click",function(){
     document.execCommand('bold');
+    // Gets the parent paragraph
     p = window.getSelection().anchorNode.parentNode.closest("p");
+    // Wraps all the content in a span to take it out of the scope of sortable
     $(p).wrapInner("<span></span>");
-    $(".tooltip").hide();
-    console.log("You just pressed bold");
+    $(".tooltip").hide(); // Hides the toolbar
   });
 
+  // Underlines selected text
   $("#underline").on("click",function(){
     document.execCommand('underline');
+    // Gets the parent paragraph
     p = window.getSelection().anchorNode.parentNode.closest("p");
+    // Wraps all the content in a span to take it out of the scope of sortable
     $(p).wrapInner("<span></span>");
-    $(".tooltip").hide();
-    console.log("You just pressed underline");
+    $(".tooltip").hide(); // Hides the toolbar
   });
 
+  // Makes selected text red
   $("#red").on("click",function(){
     document.execCommand('forecolor',false,'red');
+    // Gets the parent paragraph
     p = window.getSelection().anchorNode.parentNode.closest("p");
+    // Wraps all the content in a span to take it out of the scope of sortable
     $(p).wrapInner("<span></span>");
-    $(".tooltip").hide();
-    console.log("You just pressed red");
+    $(".tooltip").hide(); // Hides the toolbar
   });
 
+  // Extract all links
   $("#extract").on("click",function(){
-    content = $("#content").text();
-    content = "<nishant>"+content+"</nishant>";
-    links = $(content).find("a");
-    console.log(links);
-    var i=0;
-    $(".links").html("");
-    $(".links").html("<h2>Here are the links we've found...</h2>");
-    blue = true;
+    // Extracts the text and wraps it in a tag to make it valid HTML
+    content = "<nishant>"+$("#content").text()+"</nishant>";
+    links = $(content).find("a"); // Search for all <a> tags
+    var i=0; // Initialte counter variable
+    $(".links").html(""); // Empty the links container
+    $(".links").html("<h2>Here are the links we've found...</h2>"); // Adds title
+    blue = true; // Initiate color to a boolean. Plus, it rhymes
     while(i<links.length){
-      $(".links").append(links[i]);
+      $(".links").append(links[i]); // Add the link
       $(".links").append("<br/>");
       if(blue)
-        $(links[i]).css("color","red");
+        $(links[i]).css("color","red"); // If blue make the link blue
       else
-        $(links[i]).css("color","blue");
-      i+=1;
-      blue=!blue;
+        $(links[i]).css("color","blue"); // If not blue make the link red
+      i+=1; // Increment counter
+      blue=!blue; // Toggle color
     }
-    $(content).find("a").unwrap();
   });
 
+  // Gets a random word. Replaces 4 letter words
   $("#replace").on("click",function(){
+    // Sending a GET request to the endpoint
     $.get( "http://www.setgetgo.com/randomword/get.php", function( data ) {
+      // Search the text using RegEx for four letter words
+      // This method returns only the first occourence in an array.
       a = $("#content").text().match(/\b[a-zA-Z]{4}\b/);
       while(a)
       {
-        console.log(a);
-        b = $("#content").html().replace(a[0],data);
-        $("#content").html(b);
-        a = $("#content").text().match(/\b[a-zA-Z]{4}\b/);
-        console.log("ONe iterations");
+        b = $("#content").html().replace(a[0],data); // Replace the word
+        $("#content").html(b); // Update the html
+        a = $("#content").text().match(/\b[a-zA-Z]{4}\b/); // Update result array
       }
     });
   });
 });
-
-$(document).on("click keyup","#content",function(){
-  if($(this).html()=="")
-    $(this).html("<span><br></span>");
-});
-
-var boldit = function(){
-
-
-}

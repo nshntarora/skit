2dad6aff9171d54b1c87c53af55878578297513a
Nishant Arora <na.nishantarora@gmail.com>
2017-02-05 20:36:45 +0530

    Formatting and links extraction works

--- script.js
+++ script.js
@@ -3,20 +3,92 @@ $(document).on('click keyup',"#content",function(e){
           //document.execCommand('insertHTML', false, '<p></p>');
       //return false;
       e.preventDefault();
-      $("#content").removeClass("ui-sortable");
+      //if($(".default").remove()){
+      //  console.log("P");
+        $(".placeholder").remove();
+      //}
+      //else
+      //  console.log("A");
       if($('#content').html()=="") {
         console.log("empty");
-        document.execCommand('insertHTML', false, '<p></p>');
+        $("#content").html("<p><span><br></span></p>");
       }
-      document.execCommand('defaultParagraphSeparator', false, 'p');
-
-});
-$(document).on('dblclick',"#content",function(e){
-      e.preventDefault();
-      $("#content").addClass("ui-sortable");
+      var text = window.getSelection();
+      var ol = "";
+      if(text.toString()!=""){
+        oRange = text.getRangeAt(0);
+        oRect = oRange.getBoundingClientRect();
+        //$("body").append("<div class='tooltip'>This is a tooltip</div>");
+        $(".tooltip").css("top",oRect.top-50);
+        $(".tooltip").css("left",oRect.left-10);
+        $(".tooltip").show();
+      }
+      else
+        $(".tooltip").hide();
 });
 //document.execCommand('defaultParagraphSeparator', false, 'p');
 $(document).ready(function(){
   console.log("doc ready");
-  $('#content').sortable();
+  $('#content').sortable({
+    revert: true,
+    cancel: "#content p span"
+  });
+  //$('#content').enableSelection();
+  $(document).on("click","p",function(e){
+    e.preventDefault();
+    $("#content").focus();
+  });
+
+  $("#bold").on("click",function(){
+    document.execCommand('bold');
+    p = window.getSelection().anchorNode.parentNode.closest("p");
+    $(p).wrapInner("<span></span>");
+    console.log("You just pressed bold");
+  });
+
+  $("#underline").on("click",function(){
+    document.execCommand('underline');
+    p = window.getSelection().anchorNode.parentNode.closest("p");
+    $(p).wrapInner("<span></span>");
+    console.log("You just pressed underline");
+  });
+
+  $("#red").on("click",function(){
+    document.execCommand('forecolor',false,'red');
+    p = window.getSelection().anchorNode.parentNode.closest("p");
+    $(p).wrapInner("<span></span>");
+    console.log("You just pressed red");
+  });
+
+  $("#extract").on("click",function(){
+    content = $("#content").text();
+    content = "<nishant>"+content+"</nishant>";
+    links = $(content).find("a");
+    console.log(links);
+    var i=0;
+    $(".links").html("");
+    blue = true;
+    while(i<links.length){
+      $(".links").append(links[i]);
+      $(".links").append("<br/>");
+      if(blue)
+        $(links[i]).css("color","red");
+      else
+        $(links[i]).css("color","blue");
+      i+=1;
+      blue=!blue;
+    }
+    $(content).find("a").unwrap();
+  });
+
 });
+
+$(document).on("click keyup","#content",function(){
+  if($(this).html()=="")
+    $(this).html("<span><br></span>");
+});
+
+var boldit = function(){
+
+
+}
